28cc57719147b88c8837845cdb9701695c54da8f
henryquu <malve1275@gmail.com>
2022-12-14 22:43:23 +0100

    Working on move function, added binds and improved Cell

--- gui.py
+++ gui.py
@@ -4,14 +4,18 @@ from itertools import product
 from constants import *
 
 class Cell():
+    font = ('Helvetica','30','bold')
+
     def __init__(self, master, row, column):
         self.row = row
         self.column = column
         self.master = master
-        self._val = 0
+        self.text = None
 
         self.x = column * (100 + 5) + 12
         self.y = row * (100 + 5) + 12
+
+        self.val = 0
 
         y, x = self.y, self.x
         self.master.create_rectangle(x, y, x + 100, y + 100)
@@ -22,25 +26,33 @@ class Cell():
         return self._val
 
     @val.setter
-    def set_val(self, new_val):
+    def val(self, new_val):
         self._val = new_val
         self.update_text()
+        self.update_color()
 
     def update_text(self):
+        if self.text:
+            self.master.delete(self.text)
         y, x = self.y + 50, self.x + 50
-        self.master.create_text(x, y, text=self.val)
-        self.update_color()
+        self.text = self.master.create_text(x, y, text=self.val, font=Cell.font)
 
     def update_color(self):
         self.master.itemconfig(self, fill=BACKGROUNDS[self.val])
 
+
 class Board(tk.Canvas):
     def __init__(self, root, width=4):
         tk.Canvas.__init__(self, root, bg=BG_COLOR)
+
         self.width= width
+
         self.cells = np.full((width, width), Cell)
         self.init_cells()
         self.randomly_add_2or4()
+
+        self.set_binds()
+
 
     def init_cells(self):
         for y in range(self.width):
@@ -57,62 +69,49 @@ class Board(tk.Canvas):
         if spaces:
             i = int(np.random.choice(a = len(spaces)))
             y, x = spaces[i]
-            self.cells[y][x]= np.random.choice([2, 4], p = [0.9, 0.1])
+            self.cells[y][x].val = np.random.choice([2, 4], p = [0.9, 0.1])
             return True
         else:
             print('Game Over!')
             return False
 
+    def set_binds(self):
+        self.bind_all('<KeyRelease>', lambda a: self.move(a.keysym))
 
-    # not done yet
-    def transpose(self):
-        for y in range(self.width):
-            for x in range(y):
-                self.cells[y][x], self.cells[x][y] = self.cells[x][y], self.cells[y][x] 
-    
-    def reverse_columns(self):
-        for column in self.cells:
-            column.reverse()
-
-    def move(self):
-        while (key := input("Input: ")).lower() not in ['w', 'a', 's', 'd']:
-            pass
+    # fixing this function
+    def move(self, key):
+        if key.lower() not in ['w', 'a', 's', 'd']:
+            return False
 
         # down = transpose + right + transpose
         # up = transpose  + left + transpose
         if key in 'WwSs':
-            self.transpose()
+            np.transpose(self.cells)
         if key in 'WwAa':
-            self.reverse_columns()
+            np.fliplr(self.cells)
 
-        repeatIfNotMoved = True # check whether board moved at all, if not -> repeat whole function
         setOfAdded = set() # added numbers can't be added to anything else in the same movement
         while True:
             moved = False # check if board changed in this set of iterations, if not -> break loop
             for y in range(self.width): 
                 for x in range(self.width - 1):
-                    if self.board[y][x] != 0:
-                        if self.cells[y][x] == self.cells[y][x + 1] and ((y, x) or (y, x + 1)) not in setOfAdded:
-                            self.cells[y][x + 1], self.cells[y][x] = self.cells[y][x + 1]*2, 0
+                    if self.cells[y][x].val != 0:
+                        if self.cells[y][x].val == self.cells[y][x + 1].val and ((y, x) or (y, x + 1)) not in setOfAdded:
+                            self.cells[y][x + 1].val, self.cells[y][x].val = self.cells[y][x + 1].val*2, 0
                             moved = True
                             setOfAdded.update([(y, x), (y, x + 1)])
-                        elif self.cells[y][x + 1] == 0:
-                            self.cells[y][x + 1], self.cells[y][x] = self.cells[y][x], 0
+                        elif self.cells[y][x + 1].val == 0:
+                            self.cells[y][x + 1].val, self.cells[y][x].val = self.cells[y][x].val, 0
                             moved = True
                             if (y, x) in setOfAdded:
                                 setOfAdded.add((y, x + 1))
             if not moved:
                 break   
-            else: 
-                repeatIfNotMoved = False
 
         if key in 'WwAa':
-            self.reverse_columns()
+            np.fliplr(self.cells)
         if key in 'WwSs':
-            self.transpose()
-        if repeatIfNotMoved:
-            self.move()
-
+            np.transpose(self.cells)
 
 class Game(tk.Tk):
     def __init__(self):
